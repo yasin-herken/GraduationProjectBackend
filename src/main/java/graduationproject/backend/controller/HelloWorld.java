e5134d96322cc135ee176421849220d172159afe
yasinherken <yasin.herken123@gmail.com>
2022-11-27 02:21:34 +0300

    Update maven

---
+++ src/main/java/graduationproject/backend/controller/HelloWorld.java
@@ -0,0 +1,18 @@
+package graduationproject.backend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/")
+public class HelloWorld {
+    @GetMapping
+    String getMaps(){
+
+        return "sada";
+    }
+}
