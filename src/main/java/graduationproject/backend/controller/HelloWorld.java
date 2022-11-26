03c21470aba00315af7f4e5d9375a62771f9bba6
yasinherken <yasin.herken123@gmail.com>
2022-11-27 02:25:44 +0300

    REstController

--- src/main/java/graduationproject/backend/controller/HelloWorld.java
+++ src/main/java/graduationproject/backend/controller/HelloWorld.java
@@ -1,6 +1,4 @@
 package graduationproject.backend.controller;
-
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/")
 public class HelloWorld {
     @GetMapping
     String getMaps(){
