a0ea717bd9288c3030ee40fd64afa1c5dee6cf89
yasinherken <yasin.herken123@gmail.com>
2022-11-27 12:19:28 +0300

    testing

--- src/main/java/graduationproject/backend/controller/HelloWorld.java
+++ src/main/java/graduationproject/backend/controller/HelloWorld.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorld {
     @GetMapping
     String getMaps(){
-
+        System.out.println("here");
         return "sada";
     }
 }
